cfd9f920f51f69a40653c3a7565264df2c0bbb1f
Shavandi <ehsan.shavandi@gmail.com>
2023-05-25 20:52:44 +0330

    documented using doxygen

--- src/customstring.hpp
+++ src/customstring.hpp
@@ -7,24 +7,75 @@
 using namespace std;
 
 class CustomString {
+  /*! @class
+   *  @brief This class is for leverage functionality of std::string class.
+   *
+   *
+   *  This class has a m_data private member and use composition relationship
+   *  std::string does not have virtual destructor and virtual member function
+   *  and that is why I use composition relationship
+   */
  public:
+  /**
+   * A constructor for c-style string(char*) input
+   * @param cStr a char const*
+   */
   explicit CustomString(char const* cStr) : m_data(cStr) {}
+  /**
+   * A constructor for standard string in STL
+   * @param str a const string&
+   */
   explicit CustomString(const string& str) : m_data(str) {}
 
+  /**
+   * @brief toLowerCase
+   * @param str a const string& str
+   * @return std::string
+   */
   static string toLowerCase(const string& str);
+  /**
+   * @brief toLowerCase
+   * @return std::string
+   */
   string toLowerCase();
+  /**
+   * @brief toUpperCase
+   * @param str a const string&
+   * @return std::string
+   */
   static string toUpperCase(const string& str);
+  /**
+   * @brief toUpperCase
+   * @return std::string
+   */
   string toUpperCase();
 
+  /**
+   * @brief append
+   * @param str a const std::string& str
+   */
   void append(const std::string& str);
+  /**
+   * @brief length
+   * @return size_t
+   */
   size_t length() const;
 
+  /**
+   * @brief operator []
+   * @param index a size_t
+   * @return char&
+   */
   char& operator[](size_t index);
 
+  /**
+   * @brief data
+   * @return std::string
+   */
   string data() const;
 
  private:
-  string m_data;
+  string m_data; /**< std::string */
 };
 
 inline string CustomString::data() const { return m_data; }
