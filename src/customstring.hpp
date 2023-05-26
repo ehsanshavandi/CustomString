49ed04c8fd0a8b7ecb71cf1e58f92ac5ae9e58c8
Ehsan shavandi <ehsan.shavandi@gmail.com>
2023-05-26 15:13:57 +0330

    add replace member function

--- src/customstring.hpp
+++ src/customstring.hpp
@@ -1,121 +1,166 @@
 // CustomString.hpp
 
 #include <algorithm>
+#include <exception>
 #include <iostream>
 #include <regex>
 #include <string>
 using namespace std;
 
-class CustomString {
-  /*! @class
-   *  @brief This class is for leverage functionality of std::string class.
-   *
-   *
-   *  This class has a m_data private member and use composition relationship
-   *  std::string does not have virtual destructor and virtual member function
-   *  and that is why I use composition relationship
-   */
- public:
-  /**
-   * A constructor for c-style string(char*) input
-   * @param cStr a char const*
-   */
-  explicit CustomString(char const* cStr) : m_data(cStr) {}
-  /**
-   * A constructor for standard string in STL
-   * @param str a const string&
-   */
-  explicit CustomString(const string& str) : m_data(str) {}
+class CustomString
+{
+    /*! @class
+     *  @brief This class is for leverage functionality of std::string class.
+     *
+     *
+     *  This class has a m_data private member and use composition relationship
+     *  std::string does not have virtual destructor and virtual member function
+     *  and that is why I use composition relationship
+     */
+public:
+    /**
+     * A constructor for c-style string(char*) input
+     * @param cStr a char const*
+     */
+    explicit CustomString(char const* cStr) : m_data(cStr)
+    {
+    }
+    /**
+     * A constructor for standard string in STL
+     * @param str a const string&
+     */
+    explicit CustomString(const string& str) : m_data(str)
+    {
+    }
+    /**
+     * @brief append
+     * @param str a const std::string& str
+     */
+    void append(const std::string& str);
+    /**
+     * @brief length
+     * @return size_t
+     */
+    size_t length() const;
 
-  /**
-   * @static
-   * @brief toLowerCase
-   * @param str a const string& str
-   * @return std::string
-   */
-  static string toLowerCase(const string& str);
-  /**
-   * @brief toLowerCase
-   * @return std::string
-   */
-  string toLowerCase();
-  /**
-   * @static
-   * @brief toUpperCase
-   * @param str a const string&
-   * @return std::string
-   */
-  static string toUpperCase(const string& str);
-  /**
-   * @brief toUpperCase
-   * @return std::string
-   */
-  string toUpperCase();
+    /**
+     * @brief operator []
+     * @param index a size_t
+     * @return char&
+     */
+    char& operator[](size_t index);
 
-  /**
-   * @brief append
-   * @param str a const std::string& str
-   */
-  void append(const std::string& str);
-  /**
-   * @brief length
-   * @return size_t
-   */
-  size_t length() const;
+    /**
+     * @brief data
+     * @return std::string
+     */
+    string data() const;
 
-  /**
-   * @brief operator []
-   * @param index a size_t
-   * @return char&
-   */
-  char& operator[](size_t index);
+    /**
+     * @static
+     * @brief toLowerCase
+     * @param str a const string& str
+     * @return std::string
+     */
+    static string toLowerCase(const string& str);
+    /**
+     * @brief toLowerCase
+     * @return std::string
+     */
+    string toLowerCase();
+    /**
+     * @static
+     * @brief toUpperCase
+     * @param str a const string&
+     * @return std::string
+     */
+    static string toUpperCase(const string& str);
+    /**
+     * @brief toUpperCase
+     * @return std::string
+     */
+    string toUpperCase();
 
-  /**
-   * @brief data
-   * @return std::string
-   */
-  string data() const;
+    /**
+     * @brief replace
+     * @param str1 a const char*
+     * @param str2 a const char*
+     */
+    void replace(const char* str1, const char* str2) noexcept(false);
 
- private:
-  string m_data; /**< std::string */
+private:
+    string m_data; /**< std::string */
 };
 
-inline string CustomString::data() const { return m_data; }
-
-string CustomString::toLowerCase(const string& str) {
-  string tmp;
-  transform(str.begin(), str.end(), back_inserter<string>(tmp),
-            [](unsigned char c) { return std::tolower(c); });
-
-  return tmp;
+inline string CustomString::data() const
+{
+    return m_data;
 }
 
-string CustomString::toLowerCase() {
-  string tmp;
-  transform(m_data.begin(), m_data.end(), back_inserter<string>(tmp),
-            [](unsigned char c) { return std::tolower(c); });
+string CustomString::toLowerCase(const string& str)
+{
+    string tmp;
+    transform(str.begin(),
+        str.end(),
+        back_inserter<string>(tmp),
+        [](unsigned char c) { return std::tolower(c); });
 
-  return tmp;
+    return tmp;
 }
 
-string CustomString::toUpperCase(const string& str) {
-  string tmp;
-  transform(str.begin(), str.end(), back_inserter<string>(tmp),
-            [](unsigned char c) { return std::toupper(c); });
+string CustomString::toLowerCase()
+{
+    string tmp;
+    transform(m_data.begin(),
+        m_data.end(),
+        back_inserter<string>(tmp),
+        [](unsigned char c) { return std::tolower(c); });
 
-  return tmp;
+    return tmp;
 }
 
-string CustomString::toUpperCase() {
-  string tmp;
-  transform(m_data.begin(), m_data.end(), back_inserter<string>(tmp),
-            [](unsigned char c) { return std::toupper(c); });
+string CustomString::toUpperCase(const string& str)
+{
+    string tmp;
+    transform(str.begin(),
+        str.end(),
+        back_inserter<string>(tmp),
+        [](unsigned char c) { return std::toupper(c); });
 
-  return tmp;
+    return tmp;
 }
 
-inline void CustomString::append(const string& str) { m_data += str; }
+string CustomString::toUpperCase()
+{
+    string tmp;
+    transform(m_data.begin(),
+        m_data.end(),
+        back_inserter<string>(tmp),
+        [](unsigned char c) { return std::toupper(c); });
 
-inline size_t CustomString::length() const { return m_data.length(); }
+    return tmp;
+}
 
-inline char& CustomString::operator[](size_t index) { return m_data[index]; }
+void CustomString::replace(const char* str1, const char* str2)
+{
+    size_t pos = m_data.find(str1);
+    if (pos != string::npos)
+        m_data.replace(pos, string(str1).length(), str2);
+    else
+        throw runtime_error(string("Not Found ") + string(str1));
+}
+
+inline void CustomString::append(const string& str)
+{
+    m_data += str;
+}
+
+inline size_t CustomString::length() const
+{
+    return m_data.length();
+}
+
+inline char& CustomString::operator[](size_t index)
+{
+    return m_data[index];
+}
