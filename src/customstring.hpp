852be195ec9601e62c3355a5cae39cf54a248109
Shavandi <ehsan.shavandi@gmail.com>
2023-05-29 10:15:49 +0330

    add + operator add extraction operator

--- src/customstring.hpp
+++ src/customstring.hpp
@@ -5,313 +5,299 @@
 #include <functional>
 #include <iostream>
 #include <iterator>
+#include <ostream>
 #include <regex>
 #include <string>
 
-class CustomString
-{
-    /*! @class
-     *  @brief This class is for leverage functionality of std::string class.
-     *
-     *
-     *  This class has a m_data private member and use composition relationship
-     *  std::string does not have virtual destructor and virtual member function
-     *  and that is why I use composition relationship
-     */
-public:
-    /**
-     * A constructor for c-style string(char*) input
-     * @param cStr a char const*
-     */
-    explicit CustomString(char const* cStr) : m_data(cStr)
-    {
-    }
-    /**
-     * A constructor for standard string in STL
-     * @param str a const string&
-     */
-    explicit CustomString(const std::string& str) : m_data(str)
-    {
-    }
-    /**
-     * @brief append
-     * @param str a const std::string& str
-     */
-    void append(const std::string& str);
-    /**
-     * @brief length
-     * @return size_t
-     */
-    size_t length() const;
+class CustomString {
+  /*! @class
+   *  @brief This class is for leverage functionality of std::string class.
+   *
+   *
+   *  This class has a m_data private member and use composition relationship
+   *  std::string does not have virtual destructor and virtual member function
+   *  and that is why I use composition relationship
+   */
+ public:
+  /**
+   * A constructor for c-style string(char*) input
+   * @param cStr a char const*
+   */
+  explicit CustomString(char const* cStr) : m_data(cStr) {}
+  /**
+   * A constructor for standard string in STL
+   * @param str a const string&
+   */
+  explicit CustomString(const std::string& str) : m_data(str) {}
+  /**
+   * @brief append
+   * @param str a const std::string& str
+   */
+  void append(const std::string& str);
+  /**
+   * @brief length
+   * @return size_t
+   */
+  size_t length() const;
 
-    /**
-     * @brief operator []
-     * @param index a size_t
-     * @return char&
-     */
-    char& operator[](size_t index);
+  // Operator overloading
+  /**
+   * @brief operator []
+   * @param index a size_t
+   * @return char&
+   */
+  char& operator[](size_t index);
 
-    /**
-     * @brief data
-     * @return std::string
-     */
-    std::string data() const;
+  /**
+   * @brief operator +
+   * @param cs a const CustomString&
+   * @return CustomString object
+   */
+  CustomString operator+(const CustomString& cs);
+  /**
+   * @brief operator <<
+   * @param os output stream
+   * @param cs CusotmString
+   * @return output stream
+   */
+  friend std::ostream& operator<<(std::ostream& os, const CustomString& cs) {
+    os << cs.data();
+    return os;
+  }
 
-    /**
-     * @static
-     * @brief toLowerCase
-     * @param str a const string& str
-     * @return std::string
-     */
-    static std::string toLowerCase(const std::string& str);
-    /**
-     * @brief toLowerCase
-     * @return std::string
-     */
-    std::string toLowerCase();
-    /**
-     * @static
-     * @brief toUpperCase
-     * @param str a const string&
-     * @return std::string
-     */
-    static std::string toUpperCase(const std::string& str);
-    /**
-     * @brief toUpperCase
-     * @return std::string
-     */
-    std::string toUpperCase();
+  /**
+   * @brief data
+   * @return std::string
+   */
+  std::string data() const;
 
-    /**
-     * @brief replace
-     * @param oldStr a const string&
-     * @param newStr a const string&
-     */
-    void replace(const std::string& oldStr, const std::string& newStr);
-    /**
-     * @brief replaceAll
-     * @param oldStr a const string&
-     * @param newStr a const string&
-     */
-    void replaceAll(const std::string& oldStr, const std::string& newStr);
-    /**
-     * @brief replaceIgnCase
-     * @param oldStr a const string&
-     * @param newStr a const string&
-     */
-    void replaceIgnCase(const std::string& oldStr, const std::string& newStr);
-    /**
-     * @brief replaceAllIgnCase
-     * @param oldStr a const string&
-     * @param newStr a const string&
-     */
-    void replaceAllIgnCase(const std::string& oldStr, const std::string& newStr);
+  /**
+   * @static
+   * @brief toLowerCase
+   * @param str a const string& str
+   * @return std::string
+   */
+  static std::string toLowerCase(const std::string& str);
+  /**
+   * @brief toLowerCase
+   * @return std::string
+   */
+  std::string toLowerCase();
+  /**
+   * @static
+   * @brief toUpperCase
+   * @param str a const string&
+   * @return std::string
+   */
+  static std::string toUpperCase(const std::string& str);
+  /**
+   * @brief toUpperCase
+   * @return std::string
+   */
+  std::string toUpperCase();
 
-    /**
-     * @brief oldFind
-     * @param str is const string&
-     * @return position which found
-     */
-    std::size_t oldFind(const std::string& str);
-    /**
-     * @brief oldFindIgnorCase
-     * @param str is a const string&
-     * @return position which found
-     */
-    std::size_t oldFindIgnorCase(const std::string& str);
-    /**
-     * @brief findBoyer
-     * boyer moor horsepol algorithm is used
-     * @param str is a const string&
-     * @return position which found
-     */
-    std::size_t findBoyer(const std::string& str);
-    /**
-     * @brief findPattern
-     * @param pattern const std::regex& that is Raw-string
-     * @return  std::vector<std::string>
-     */
-    std::vector<std::string> findPattern(const std::regex& pattern);
+  /**
+   * @brief replace
+   * @param oldStr a const string&
+   * @param newStr a const string&
+   */
+  void replace(const std::string& oldStr, const std::string& newStr);
+  /**
+   * @brief replaceAll
+   * @param oldStr a const string&
+   * @param newStr a const string&
+   */
+  void replaceAll(const std::string& oldStr, const std::string& newStr);
+  /**
+   * @brief replaceIgnCase
+   * @param oldStr a const string&
+   * @param newStr a const string&
+   */
+  void replaceIgnCase(const std::string& oldStr, const std::string& newStr);
+  /**
+   * @brief replaceAllIgnCase
+   * @param oldStr a const string&
+   * @param newStr a const string&
+   */
+  void replaceAllIgnCase(const std::string& oldStr, const std::string& newStr);
 
-private:
-    std::string m_data; /**< std::string */
+  /**
+   * @brief oldFind
+   * @param str is const string&
+   * @return position which found
+   */
+  std::size_t oldFind(const std::string& str);
+  /**
+   * @brief oldFindIgnorCase
+   * @param str is a const string&
+   * @return position which found
+   */
+  std::size_t oldFindIgnorCase(const std::string& str);
+  /**
+   * @brief findBoyer
+   * boyer moor horsepol algorithm is used
+   * @param str is a const string&
+   * @return position which found
+   */
+  std::size_t findBoyer(const std::string& str);
+  /**
+   * @brief findPattern
+   * @param pattern const std::regex& that is Raw-string
+   * @return  std::vector<std::string>
+   */
+  std::vector<std::string> findPattern(const std::regex& pattern);
 
-    /**
-     * @brief m_srearchString a private member funtion
-     * which used for searching character by character
-     * @param ref is a main string
-     * @param str is a wanted string
-     * @param pred predicte function
-     * @return std::string::const_iterator
-     */
-    std::string::const_iterator m_srearchString(const std::string& ref,
-        const std::string& str,
-        std::function<bool(unsigned char, unsigned char)> pred = std::equal_to<unsigned char>());
+ private:
+  std::string m_data; /**< std::string */
 
-    /**
-     * @brief predicate function for searching in string case insensitively
-     */
-    std::function<bool(unsigned char, unsigned char)> m_equal_iggnore_case =
-        [](unsigned char rc, unsigned char sc) -> bool { return tolower(rc) == tolower(sc); };
+  /**
+   * @brief m_srearchString a private member funtion
+   * which used for searching character by character
+   * @param ref is a main string
+   * @param str is a wanted string
+   * @param pred predicte function
+   * @return std::string::const_iterator
+   */
+  std::string::const_iterator m_srearchString(
+      const std::string& ref, const std::string& str,
+      std::function<bool(unsigned char, unsigned char)> pred =
+          std::equal_to<unsigned char>());
+
+  /**
+   * @brief predicate function for searching in string case insensitively
+   */
+  std::function<bool(unsigned char, unsigned char)> m_equal_iggnore_case =
+      [](unsigned char rc, unsigned char sc) -> bool {
+    return tolower(rc) == tolower(sc);
+  };
 };
 
-inline std::string CustomString::data() const
-{
-    return m_data;
+inline std::string CustomString::data() const { return m_data; }
+
+std::string CustomString::toLowerCase(const std::string& str) {
+  std::string tmp;
+  transform(str.begin(), str.end(), std::back_inserter<std::string>(tmp),
+            [](unsigned char c) { return std::tolower(c); });
+
+  return tmp;
 }
 
-std::string CustomString::toLowerCase(const std::string& str)
-{
-    std::string tmp;
-    transform(str.begin(),
-        str.end(),
-        std::back_inserter<std::string>(tmp),
-        [](unsigned char c) { return std::tolower(c); });
+std::string CustomString::toLowerCase() {
+  std::string tmp;
+  transform(m_data.begin(), m_data.end(), std::back_inserter<std::string>(tmp),
+            [](unsigned char c) { return std::tolower(c); });
 
-    return tmp;
+  return tmp;
 }
 
-std::string CustomString::toLowerCase()
-{
-    std::string tmp;
-    transform(m_data.begin(),
-        m_data.end(),
-        std::back_inserter<std::string>(tmp),
-        [](unsigned char c) { return std::tolower(c); });
+std::string CustomString::toUpperCase(const std::string& str) {
+  std::string tmp;
+  transform(str.begin(), str.end(), std::back_inserter<std::string>(tmp),
+            [](unsigned char c) { return std::toupper(c); });
 
-    return tmp;
+  return tmp;
 }
 
-std::string CustomString::toUpperCase(const std::string& str)
-{
-    std::string tmp;
-    transform(str.begin(),
-        str.end(),
-        std::back_inserter<std::string>(tmp),
-        [](unsigned char c) { return std::toupper(c); });
+std::string CustomString::toUpperCase() {
+  std::string tmp;
+  transform(m_data.begin(), m_data.end(), std::back_inserter<std::string>(tmp),
+            [](unsigned char c) { return std::toupper(c); });
 
-    return tmp;
+  return tmp;
 }
 
-std::string CustomString::toUpperCase()
-{
-    std::string tmp;
-    transform(m_data.begin(),
-        m_data.end(),
-        std::back_inserter<std::string>(tmp),
-        [](unsigned char c) { return std::toupper(c); });
-
-    return tmp;
+void CustomString::replace(const std::string& oldStr,
+                           const std::string& newStr) {
+  size_t pos = m_data.find(oldStr);
+  if (pos != std::string::npos)
+    m_data.replace(pos, std::string(oldStr).length(), newStr);
 }
 
-void CustomString::replace(const std::string& oldStr, const std::string& newStr)
-{
-    size_t pos = m_data.find(oldStr);
-    if (pos != std::string::npos)
-        m_data.replace(pos, std::string(oldStr).length(), newStr);
+void CustomString::replaceAll(const std::string& oldStr,
+                              const std::string& newStr) {
+  size_t pos = 0;
+  while (pos != m_data.length() - 1) {
+    pos = m_data.find(oldStr);
+    if (pos != std::string::npos) {
+      auto needleLen = std::string(oldStr).length();
+      m_data.replace(pos, needleLen, newStr);
+      pos += needleLen;
+    } else
+      break;
+  }
 }
 
-void CustomString::replaceAll(const std::string& oldStr, const std::string& newStr)
-{
-    size_t pos = 0;
-    while (pos != m_data.length() - 1)
-    {
-        pos = m_data.find(oldStr);
-        if (pos != std::string::npos)
-        {
-            auto needleLen = std::string(oldStr).length();
-            m_data.replace(pos, needleLen, newStr);
-            pos += needleLen;
-        }
-        else
-            break;
-    }
+void CustomString::replaceIgnCase(const std::string& oldStr,
+                                  const std::string& newStr) {
+  size_t pos = this->oldFindIgnorCase(oldStr);
+  if (pos != std::string::npos)
+    m_data.replace(pos, std::string(oldStr).length(), newStr);
 }
 
-void CustomString::replaceIgnCase(const std::string& oldStr, const std::string& newStr)
-{
-    size_t pos = this->oldFindIgnorCase(oldStr);
-    if (pos != std::string::npos)
-        m_data.replace(pos, std::string(oldStr).length(), newStr);
+void CustomString::replaceAllIgnCase(const std::string& oldStr,
+                                     const std::string& newStr) {
+  size_t pos = 0;
+  while (pos != m_data.length() - 1) {
+    pos = this->oldFindIgnorCase(oldStr);
+    if (pos != std::string::npos) {
+      auto needleLen = std::string(oldStr).length();
+      m_data.replace(pos, needleLen, newStr);
+      pos += needleLen;
+    } else
+      break;
+  }
 }
 
-void CustomString::replaceAllIgnCase(const std::string& oldStr, const std::string& newStr)
-{
-    size_t pos = 0;
-    while (pos != m_data.length() - 1)
-    {
-        pos = this->oldFindIgnorCase(oldStr);
-        if (pos != std::string::npos)
-        {
-            auto needleLen = std::string(oldStr).length();
-            m_data.replace(pos, needleLen, newStr);
-            pos += needleLen;
-        }
-        else
-            break;
-    }
+std::size_t CustomString::oldFind(const std::string& str) {
+  std::string::const_iterator it = m_srearchString(m_data, str);
+  if (it != m_data.end())
+    return std::distance(m_data.cbegin(), it);
+  else
+    return std::string::npos;
 }
 
-std::size_t CustomString::oldFind(const std::string& str)
-{
-    std::string::const_iterator it = m_srearchString(m_data, str);
-    if (it != m_data.end())
-        return std::distance(m_data.cbegin(), it);
-    else
-        return std::string::npos;
+size_t CustomString::oldFindIgnorCase(const std::string& str) {
+  std::string::const_iterator it =
+      m_srearchString(m_data, str, m_equal_iggnore_case);
+  if (it != m_data.end())
+    return std::distance(m_data.cbegin(), it);
+  else
+    return std::string::npos;
 }
 
-size_t CustomString::oldFindIgnorCase(const std::string& str)
-{
-    std::string::const_iterator it = m_srearchString(m_data, str, m_equal_iggnore_case);
-    if (it != m_data.end())
-        return std::distance(m_data.cbegin(), it);
-    else
-        return std::string::npos;
+size_t CustomString::findBoyer(const std::string& str) {
+  auto it =
+      std::search(m_data.begin(), m_data.end(),
+                  std::boyer_moore_horspool_searcher(str.begin(), str.end()));
+  if (it != m_data.end())
+    return std::distance(m_data.begin(), it);
+  else
+    return std::string::npos;
 }
 
-size_t CustomString::findBoyer(const std::string& str)
-{
-    auto it = std::search(
-        m_data.begin(), m_data.end(), std::boyer_moore_horspool_searcher(str.begin(), str.end()));
-    if (it != m_data.end())
-        return std::distance(m_data.begin(), it);
-    else
-        return std::string::npos;
+std::vector<std::string> CustomString::findPattern(const std::regex& pattern) {
+  std::vector<std::string> founds;
+  std::string tmp = m_data;
+  std::smatch matcher;
+  while (regex_search(tmp, matcher, pattern)) {
+    for (auto found : matcher) founds.push_back(found);
+    tmp = matcher.suffix().str();
+  }
+  return founds;
 }
 
-std::vector<std::string> CustomString::findPattern(const std::regex& pattern)
-{
-    std::vector<std::string> founds;
-    std::string tmp = m_data;
-    std::smatch matcher;
-    while (regex_search(tmp, matcher, pattern))
-    {
-        for (auto found : matcher)
-            founds.push_back(found);
-        tmp = matcher.suffix().str();
-    }
-    return founds;
+std::string::const_iterator CustomString::m_srearchString(
+    const std::string& ref, const std::string& str,
+    std::function<bool(unsigned char, unsigned char)> pred) {
+  return search(ref.begin(), ref.end(), str.begin(), str.end(), pred);
 }
 
-std::string::const_iterator CustomString::m_srearchString(const std::string& ref,
-    const std::string& str,
-    std::function<bool(unsigned char, unsigned char)> pred)
-{
-    return search(ref.begin(), ref.end(), str.begin(), str.end(), pred);
-}
+inline void CustomString::append(const std::string& str) { m_data += str; }
 
-inline void CustomString::append(const std::string& str)
-{
-    m_data += str;
-}
+inline size_t CustomString::length() const { return m_data.length(); }
 
-inline size_t CustomString::length() const
-{
-    return m_data.length();
-}
+inline char& CustomString::operator[](size_t index) { return m_data[index]; }
 
-inline char& CustomString::operator[](size_t index)
-{
-    return m_data[index];
+inline CustomString CustomString::operator+(const CustomString& cs) {
+  return CustomString(this->m_data + cs.data());
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,76 +1,85 @@
-#include "customstring.hpp"
 #include <algorithm>
 #include <iostream>
 #include <sstream>
 #include <unordered_map>
 #include <vector>
 
+#include "customstring.hpp"
+
 using namespace std;
 
-int main()
-{
-    //  string example = "Ehsan SHaavnadi HellO";
-    CustomString csExample("Ehsan Shavandi Ehsan");
-    string exLower = csExample.toLowerCase();
-    string exUpper = csExample.toUpperCase();
-    //    csExample.replaceAll("Ehsan", "Ali");
-    //    csExample.replaceIgnCase("ehSan", "Ali");
-    csExample.replaceAllIgnCase("ehSan", "Ali");
-    //    cout << csExample.data() << endl;
+int main() {
+  //  string example = "Ehsan SHaavnadi HellO";
+  CustomString csExample("Ehsan Shavandi Ehsan");
+  string exLower = csExample.toLowerCase();
+  string exUpper = csExample.toUpperCase();
+  //    csExample.replaceAll("Ehsan", "Ali");
+  //    csExample.replaceIgnCase("ehSan", "Ali");
+  csExample.replaceAllIgnCase("ehSan", "Ali");
+  //    cout << csExample.data() << endl;
 
-    CustomString dna("GATATATACAGGTACCCAATTTACAGATACAAATTTTAAGGAAGAGATATACACACA");
-    //    size_t posFound = dna.oldFind("GATACA");
-    //    if (posFound != string::npos)
-    //    {
-    //        cout << "Found at " << posFound << endl;
-    //    }
+  CustomString dna("GATATATACAGGTACCCAATTTACAGATACAAATTTTAAGGAAGAGATATACACACA");
+  //    size_t posFound = dna.oldFind("GATACA");
+  //    if (posFound != string::npos)
+  //    {
+  //        cout << "Found at " << posFound << endl;
+  //    }
 
-    CustomString name("can i match my name Ehsan");
-    //    posFound = name.oldFindIgnorCase("ehsan");
-    //    if (posFound != string::npos)
-    //        cout << "Found at " << posFound << endl;
-    size_t posFound = dna.findBoyer("GATACA");
-    //    if (posFound != string::npos)
-    //    {
-    //        cout << "Found at " << posFound << endl;
-    //    }
+  CustomString name("can i match my name Ehsan");
+  //    posFound = name.oldFindIgnorCase("ehsan");
+  //    if (posFound != string::npos)
+  //        cout << "Found at " << posFound << endl;
+  size_t posFound = dna.findBoyer("GATACA");
+  //    if (posFound != string::npos)
+  //    {
+  //        cout << "Found at " << posFound << endl;
+  //    }
 
-    // REGEX
-    CustomString example(
-        "Meanwhile LLVM has released apt 09121234567 packages for LLVM 13 and 14 "
-        "but "
-        "out-of-the 0912-123-4567 box clang-format-13, 0912 123 4567 clang-tidy-13, ... are "
-        "not available "
-        "via apt-get install clang-format-13.How 0912 1111111 can I install clang-format-13 "
-        "on Ubuntu 0912-1234567 18.04?");
+  // REGEX
+  CustomString example(
+      "Meanwhile LLVM has released apt 09121234567 packages for LLVM 13 and 14 "
+      "but "
+      "out-of-the 0912-123-4567 box clang-format-13, 0912 123 4567 "
+      "clang-tidy-13, ... are "
+      "not available "
+      "via apt-get install clang-format-13.How 0912 1111111 can I install "
+      "clang-format-13 "
+      "on Ubuntu 0912-1234567 18.04?");
 
-    std::regex mobilePattern(
-        R"delim(\d{11}|\d{4}[\s,-]\d{3}[\s,-]\d{4}|\d{4}[\s,-]\d{7})delim");	// matches words
+  std::regex mobilePattern(
+      R"delim(\d{11}|\d{4}[\s,-]\d{3}[\s,-]\d{4}|\d{4}[\s,-]\d{7})delim");  // matches words
 
-    vector<string> foundsAll = example.findPattern(mobilePattern);
-    for_each(foundsAll.begin(), foundsAll.end(), [](const string& item) { cout << item << endl; });
-    //    cout << exLower << endl;
-    //    cout << exUpper << endl;
-    //    //    stringstream ss;
-    //    unordered_map<string, int> wordCount;
-    //    vector<string> myArr{"apple", "orange", "banana", "apple", "banana",
-    //    "orange", "apple", "alaki"}; for (auto it1 = myArr.begin(); it1 !=
-    //    myArr.end(); ++it1) {
-    //        auto ptrFound = wordCount.find(*it1);
-    //        if (ptrFound == wordCount.end())
-    //            wordCount[*it1] = 1;
+  vector<string> foundsAll = example.findPattern(mobilePattern);
+  for_each(foundsAll.begin(), foundsAll.end(),
+           [](const string& item) { cout << item << endl; });
 
-    //        else
-    //            continue;
-    //        for (auto it2 = it1 + 1; it2 < myArr.end(); ++it2) {
-    //            if (*it1 == *it2) {
-    //                //                cout << "it1: " << *it1 << "\t" <<
-    //                *it2 << endl; wordCount[*it1]++;
-    //            }
-    //        }
-    //    }
+  CustomString ex1("Ehsan");
+  CustomString ex2("Shavandi");
 
-    //    for (const auto &p : wordCount)
-    //        cout << p.first << "\t" << p.second << endl;
-    return 0;
+  CustomString ex3 = ex1 + ex2;
+  cout << ex3 << endl;
+  //    cout << exLower << endl;
+  //    cout << exUpper << endl;
+  //    //    stringstream ss;
+  //    unordered_map<string, int> wordCount;
+  //    vector<string> myArr{"apple", "orange", "banana", "apple", "banana",
+  //    "orange", "apple", "alaki"}; for (auto it1 = myArr.begin(); it1 !=
+  //    myArr.end(); ++it1) {
+  //        auto ptrFound = wordCount.find(*it1);
+  //        if (ptrFound == wordCount.end())
+  //            wordCount[*it1] = 1;
+
+  //        else
+  //            continue;
+  //        for (auto it2 = it1 + 1; it2 < myArr.end(); ++it2) {
+  //            if (*it1 == *it2) {
+  //                //                cout << "it1: " << *it1 << "\t" <<
+  //                *it2 << endl; wordCount[*it1]++;
+  //            }
+  //        }
+  //    }
+
+  //    for (const auto &p : wordCount)
+  //        cout << p.first << "\t" << p.second << endl;
+  return 0;
 }
